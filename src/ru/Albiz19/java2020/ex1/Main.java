49912145b9adf15d76a99a24a7f473ba46716482
Albiz19 <nikolai_ghromov@mail.ru>
2020-09-07 11:14:11 +0300

    Init 1-6

--- src/ru/Albiz19/java2020/ex1/Main.java
+++ src/ru/Albiz19/java2020/ex1/Main.java
@@ -10,23 +10,28 @@ public class Main {
     public static int num3() {
             int[] array = {100, 15, 12, 102, 156, 1000, 1, 0};
             int s=0;
+            /* //реализация на for
             for (int i = 0; i < array.length; i++) {
                 s = s + array[i];
             }
-        /*int i=0;
+            */
+        /* // реализация на while
+        int i=0;
         while (i<array.length)
         {
             s=s+array[i];
             i++;
         }
+        */
+        /* //реализация на do while
+        int i=0;
         do
         {
             s=s+array[i];
             i++;
         }
-
         while (i<array.length);
-         */
+        */
             return s;
         }
 }
